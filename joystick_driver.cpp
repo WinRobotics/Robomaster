d16192f9bc83a19f1f7499fef14bb4f1e5713062
Sherwin Fun <53950231+WinRobotics@users.noreply.github.com>
2021-12-25 02:16:11 +0800

    working prototype

--- joystick_driver.cpp
+++ joystick_driver.cpp
@@ -5,6 +5,8 @@
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/Joy.h"
 
+#include <string>
+
 
 //Forward Declaration
 serial::Serial ros_serial;
@@ -14,8 +16,9 @@ class joystick_driver
 	public:
 	joystick_driver(ros::NodeHandle *nh);
 	void init_serial_driver();
-	void drive();
+	void send_command();
 	void serial_read();
+	void set_speed(double x, double y , double z);
 	
 	
 	private:
@@ -27,12 +30,21 @@ class joystick_driver
 	const std::string _SDK_MODE = "command;";
 
 	//help function -> get controller class
-	void set_speed(float x, float y, float z);
-	float _vel_x; //Speed X
-	float _vel_y; //Speed Y
-	float _vel_z; //SPeed Pivot
 
-	
+	geometry_msgs::Twist cmd_vel;
+
+	double _vel_x = 0; //Speed X 1ms
+	double _vel_y = 0; //Speed Y 1ms
+	double _vel_z = 0; //SPeed Pivot
+
+	double _max_spd_x = 0.25;
+	double _max_spd_y = 0.25;
+	double _max_spd_z = 0.0;
+
+	std::string _spd_x = "0.00";
+	std::string _spd_y = "0.00";
+	std::string _spd_z = "0.00";
+
 
 
 	//Subscriber
@@ -75,13 +87,53 @@ void joystick_driver::init_serial_driver()
 	
 }
 
-void joystick_driver::drive()
+void joystick_driver::send_command()
 {
+
+
+
+	std::string speed_command = std::string("chassis speed")
+					+ " x " + _spd_x
+					+ " y " + _spd_y
+					+ " z " + "1.15"
+					+ ";";
+
+	ROS_INFO_STREAM(speed_command);
+	ros_serial.write(speed_command);
 
 }
 
-void joystick_driver::set_speed(float x, float y, float z)
+
+
+void joystick_driver::set_speed(double x, double y, double z)
 {
+
+	//conversion of axes to max speed (1m/s)
+	_vel_x = _max_spd_x * x;
+	_vel_y = _max_spd_y * y;
+	_vel_z = 0.00;
+	
+
+
+	//For future use, convert to cmd_vel
+	cmd_vel.linear.x = _vel_x;
+	cmd_vel.linear.y = _vel_y;
+	cmd_vel.linear.z = 0.00;
+
+	_pub.publish(cmd_vel);
+
+	if(_vel_x == -0)
+	{
+		_vel_x =0.00;
+	}
+
+	if(_vel_y == -0)
+	{
+		_vel_y =0.00;
+	}
+	_spd_x = std::to_string(_vel_x);
+	_spd_y = std::to_string(_vel_y);
+	_spd_z = std::to_string(_vel_z);
 
 }
 
@@ -102,7 +154,8 @@ void joystick_driver::serial_read()
 joystick_driver::joystick_driver(ros::NodeHandle *nh) : _nh(nh)
 {
 	//init all publisher and Subscriber
-	_pub = _nh->advertise<std_msgs::Float32>("/joystick_feedback",1);
+	//_pub = _nh->advertise<std_msgs::Float32>("/joystick_feedback",1);
+	_pub = _nh->advertise<geometry_msgs::Twist>("/cmd_vel",1);
 	_sub = _nh->subscribe("/joy",10,&joystick_driver::joy_callback,this);
 }
 
@@ -111,14 +164,12 @@ void joystick_driver::joy_callback(const sensor_msgs::Joy & joy_)
 {
 
 	//joy to low_level controller parsing
-	float x = joy_.axes[1];
-	float y = joy_.axes[0];
-	float z =0; //TODO find a way to do pivoting
+	float x = joy_.axes[1]; // -1 to 1
+	float y = joy_.axes[0]; // -1 to 1
+	float z = 0; //TODO find a way to do pivoting
 
 	//set speed
 	set_speed(x,y,z);
-	
-	
 
 }
 
@@ -135,10 +186,14 @@ int main(int argc, char ** argv)
 	joystick_driver driver(&nh);
 
 	driver.init_serial_driver();
-		
+	//driver.send_command();
+
+	ros::Rate r(1.2);
 	while(ros::ok())
 	{
 		driver.serial_read();
+		r.sleep();
+		driver.send_command();	
 		ros::spinOnce();
 	}
 	
